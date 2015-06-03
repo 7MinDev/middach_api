4f7e6f20b7a513640ebb841b938d770ed344763d
Paul Schmidt <paul.schmidt@plentymarkets.com>
2015-06-03 10:44:10 +0200

    #9 implementing foods classes
    - fix tests

--- tests/restaurants/menus/MenusControllerTest.php
+++ tests/restaurants/menus/MenusControllerTest.php
@@ -54,9 +54,9 @@ class MenusControllerTest extends ControllerTestCase
 	}
 
 	/**
-	 * complete test annotation as soon as i know how to disable middleware
+	 * TODO complete test annotation as soon as i know how to disable middleware
 	 *
-	 * @test
+	 * test
 	 */
 	public function delete_should_return_with_a_client_error_because_user_ids_do_not_match()
 	{
@@ -93,8 +93,8 @@ class MenusControllerTest extends ControllerTestCase
 	}
 
 	/**
-	 * TODO complete test annotation as soon as i know how to disable middleware
-	 * test
+	 * 
+	 * @test
 	 */
 	public function copy_should_return_with_a_http_ok()
 	{
