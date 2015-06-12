ced8f1f6a6cfa506e83dc667c35cfaa8d3c44a4c
Paul Schmidt <paul.schmidt@plentymarkets.com>
2015-06-12 08:43:31 +0200

    psr2

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -1,47 +1,50 @@
-<?php namespace App\Http\Controllers;
-/**
- * @author pschmidt
- */
+<?php
+
+namespace App\Http\Controllers;
 
 use App\Repositories\Contracts\RestaurantRepositoryContract;
 use Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ *
+ * @author pschmidt
+ */
 class RestaurantsController extends BaseController
 {
 
-	/**
-	 * @var RestaurantRepositoryContract
-	 */
-	private $restaurant;
+    /**
+     * @var RestaurantRepositoryContract
+     */
+    private $restaurant;
 
-	/**
-	 * @param RestaurantRepositoryContract $restaurant
-	 */
-	public function __construct(RestaurantRepositoryContract $restaurant)
-	{
-		$this->restaurant = $restaurant;
-		parent::__construct();
-	}
+    /**
+     * @param RestaurantRepositoryContract $restaurant
+     */
+    public function __construct(RestaurantRepositoryContract $restaurant)
+    {
+        $this->restaurant = $restaurant;
+        parent::__construct();
+    }
 
-	/**
-	 * @param null $restaurantId
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function show($restaurantId = null)
-	{
-		if ($restaurantId == null)
-		{
-			return Response::json([
-				'status' => 'error',
-				'message' => 'restaurantId param is missing'
-			], JsonResponse::HTTP_BAD_REQUEST);
-		}
+    /**
+     * @param null $restaurantId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($restaurantId = null)
+    {
+        if ($restaurantId == null) {
+            return Response::json([
+                'status' => 'error',
+                'message' => 'restaurantId param is missing'
+            ], JsonResponse::HTTP_BAD_REQUEST);
+        }
 
-		$restaurant = $this->restaurant->findById($restaurantId);
+        $restaurant = $this->restaurant->findById($restaurantId);
 
-		return Response::json([
-			'status' => 'ok',
-			'data' => $restaurant]);
-	}
+        return Response::json([
+            'status' => 'ok',
+            'data' => $restaurant
+        ]);
+    }
 }
